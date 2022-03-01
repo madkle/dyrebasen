e6f9a542283b874178788e691515df62154aa272
madkle <62890073+madkle@users.noreply.github.com>
2022-03-01 13:32:10 +0100

    added express and pg and the standard server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const server = express();
+const PORT = process.env.PORT || 8080;
+server.set("port", PORT);
+
+// middleware ---------------------------
+server.use(express.static("public"));
+server.use(express.json());
+
+//general error handlogig----------------
+server.use(function(err, req, res, next){
+	res.status(500).json({
+		error: "something went wrong on the server!",
+		descr: err
+	}).end();
+});
+server.listen(server.get("port"), function(){
+	
+});
